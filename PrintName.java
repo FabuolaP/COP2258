9034cabe0b333231861db10d59de1859dcbb4658
Fabuola Pierre <fjp15@my.fsu.edu>
2019-07-06 23:17:16 -0400

    Add files via upload

---
+++ PrintName.java
@@ -0,0 +1,10 @@
+public class PrintName {
+    
+        public static void main(String[] args) {
+            
+            //print first and last name using print ()
+            System.out.print("Fabuola Pierre"  );
+                
+        }//end of main
+        
+}//end of class
